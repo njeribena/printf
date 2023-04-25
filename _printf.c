0cd3fdd49c3477bde7099bf4e47dd22d2f02210d
njeribena <njeriwaweru94@gmail.com>
2023-04-25 11:28:17 -0700

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,43 +1,6 @@
-#include <stddef.h>
-#include <stdarg.h>
 #include "main.h"
 
-/**
- * get_op - select function for character conversion
- * @c: char to be checked
- * Return: pointer
- */
-
-int (*get_op(const char c))(va_list)
-{
-	int a = 0;
-
-	flags_p fp[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"i", print_nbr},
-		{"d", print_nbr},
-		{"b", print_binary},
-		{"o", print_octal},
-		{"x", print_hexa_lower},
-		{"X", print_hexa_upper},
-		{"u", print_unsigned},
-		{"S", print_str_unprintable},
-		{"r", print_str_reverse},
-		{"p", print_ptr},
-		{"R", print_rot13},
-		{"%", print_percent}
-	};
-	while (a < 14)
-	{
-		if (c == fp[a].c[0])
-		{
-			return (fp[a].f);
-		}
-		a++;
-	}
-	return (NULL);
-}
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Replicate behavior of printf function
