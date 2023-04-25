19778f4b92b81c32bafe1a97b5da750283ad6abd
njeribena <njeriwaweru94@gmail.com>
2023-04-25 11:42:56 -0700

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,47 +1,84 @@
+#include <stddef.h>
+#include <stdarg.h>
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+/**
+ * get_op - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
+ */
+
+int (*get_op(const char c))(va_list)
+{
+	int i = 0;
+
+	flags_p fp[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
+		{"%", print_percent}
+	};
+	while (i < 14)
+	{
+		if (c == fp[i].c[0])
+		{
+			return (fp[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
 
 /**
- * _printf - Replicate behavior of printf function
+ * _printf - Reproduce behavior of printf function
  * @format: format string
- * Return: printed chars value
+ * Return: value of printed chars
  */
 
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int sum = 0, a = 0;
+	int sum = 0, i = 0;
 	int (*func)();
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(ap, format);
 
-	while (format[a])
+	while (format[i])
 	{
-		if (format[a] == '%')
+		if (format[i] == '%')
 		{
-			if (format[a + 1] != '\0')
-				func = get_op(format[a + 1]);
+			if (format[i + 1] != '\0')
+				func = get_op(format[i + 1]);
 			if (func == NULL)
 			{
-				_putchar(format[a]);
+				_putchar(format[i]);
 				sum++;
-				a++;
+				i++;
 			}
 			else
 			{
 				sum += func(ap);
-				a += 2;
+				i += 2;
 				continue;
 			}
 		}
 		else
 		{
-			_putchar(format[a]);
+			_putchar(format[i]);
 			sum++;
-			a++;
+			i++;
 		}
 	}
 	va_end(ap);
